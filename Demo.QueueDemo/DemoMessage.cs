a4ed25e4009ec83a0eccf5a7a23eab2b4ad5d506
cunwei.fan <cunwei.fan@31huiyi.com>
2018-08-31 21:12:44 +0800

    DemoQueue

---
+++ Demo.QueueDemo/DemoMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo.QueueDemo
+{
+    /// <summary>
+    /// 消息体
+    /// </summary>
+    public class DemoMessage
+    {
+        public string BusinessType { get; set; }
+        public string BusinessId { get; set; }
+        public string Body { get; set; }
+    }
+}
---
+++ Demo.QueueDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Demo.QueueDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Factory.StartNew(() =>
+            {
+                //从队列中取元素。
+                while (!DemoQueueBlock<DemoMessage>.IsComleted())
+                {
+                    try
+                    {
+                        var m = DemoQueueBlock<DemoMessage>.Take();
+                        Console.WriteLine("已消费:" + m.BusinessId);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            });
+            //添加元素
+            while (true)
+            {
+                Console.WriteLine("请输入队列");
+                var read = Console.ReadLine();
+                if (read == "exit")
+                {
+                    return;
+                }
+
+                DemoQueueBlock<DemoMessage>.Add(new DemoMessage() { BusinessId = read });
+            }
+        }
+    }
+}
